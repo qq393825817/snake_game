9df7de853f92a420a5252b0a7591ae0568c00400
qq393825817 <xiyou.jike1101lw@gmail.com>
2013-11-28 22:39:44 +0800

    贪吃蛇基本功能实现

--- snake.asm
+++ snake.asm
@@ -46,11 +46,11 @@ data segment
     snake18     db    0ah, 0dh, "************************************************************$";|
 ;------------------------------------------------------------------------------------------------
 ;音频信息——节拍和频率
-	mus dw 262, 294, 300, -1	
-	time dw 2, 2, 2
+    mus         dw    262, 294, 300, -1    
+    time        dw    2, 2, 2
 ;--------------
-	music_si dw 0
-	music_di dw 0
+    music_si    dw    0
+    music_di    dw    0
 ;--------------
 ;表示蛇的坐标
     s_locate    dw    546    dup(1)
@@ -64,8 +64,8 @@ data segment
 ;表示所得分数
     score       dw    0
     char_score  db    0,0,0
-	food_x      db    (?)
-	food_y      db    (?)
+    food_x      db    (?)
+    food_y      db    (?)
 ;coordinate_x表示列，coordinate_y表示行
     coordinate_x db   (?)
     coordinate_y db   (?)
@@ -106,81 +106,109 @@ start:
 ;--------------------------------------------------
 ;music open
 music:
-	push cx
-	push bx
-p:	
-	mov si,music_si[0]
-	mov di,music_di[0]
-	mov cx,mus[si]
-	cmp cx, -1
-	je p
-	mov bx,time[di]
-	call sound
-	add si, 2			; 取下一频率值
-	add di, 2			; 取下一时间节拍值
-	cmp si, 4
-	jne q
-	mov si, 0
-	mov di, 0
-q:	
-	mov music_di[0], di
-	mov music_si[0], si
-	pop bx
-	pop cx
-	ret
+    push cx
+    push bx
+p:    
+    mov si,music_si[0]
+    mov di,music_di[0]
+    mov cx,mus[si]
+    cmp cx, -1
+    je p
+    mov bx,time[di]
+    call sound
+    add si, 2            ; 取下一频率值
+    add di, 2            ; 取下一时间节拍值
+    cmp si, 4
+    jne q
+    mov si, 0
+    mov di, 0
+q:    
+    mov music_di[0], di
+    mov music_si[0], si
+    pop bx
+    pop cx
+    ret
 
 sound:
-	push dx
-	mov al, 0b6h		; 向计数器写控制字
-						; 方式3、双字节写和二进制计数方式写到控制口
-	out 43h, al			; 公用的控制寄存器（I/O 端口 43H）
-	mov dx, 08h
-	mov ax, 3208h
-	div cx				; 除以频率，其商 ax 为计数值
-						; 计数器2（I/O 端口 42H）用来控制扬声器发生
-	out 42h, al			; 计数值先送低 8 位
-	mov al, ah
-	out 42h, al			; 计数值后送高 8 位
-	in al, 61h
-	mov ah, al
-	or al, 3
-	out 61h, al
+    push dx
+    mov al, 0b6h        ; 向计数器写控制字
+                        ; 方式3、双字节写和二进制计数方式写到控制口
+    out 43h, al            ; 公用的控制寄存器（I/O 端口 43H）
+    mov dx, 08h
+    mov ax, 3208h
+    div cx                ; 除以频率，其商 ax 为计数值
+                        ; 计数器2（I/O 端口 42H）用来控制扬声器发生
+    out 42h, al            ; 计数值先送低 8 位
+    mov al, ah
+    out 42h, al            ; 计数值后送高 8 位
+    in al, 61h
+    mov ah, al
+    or al, 3
+    out 61h, al
 l:
-	push dx
-	push ax
-	mov dx, 8h
-	mov ax, 0f05h
+    push dx
+    push ax
+    mov dx, 8h
+    mov ax, 0f05h
 
 s:
-	sub ax, 1
-	sbb dx, 0			; 这条语句表示只要 ax 不错位，那么 CF 就等于 0，dx - CF = dx 不影响 dx 的值。当 ax 减没了，错位了，dx 才减少 1
-	jnz s
-	pop ax
-	pop dx
-	dec bx
-	jnz l
-	mov al, ah
-	out 61h, al
-	pop dx
-	ret
+    sub ax, 1
+    sbb dx, 0            ; 这条语句表示只要 ax 不错位，那么 CF 就等于 0，dx - CF = dx 不影响 dx 的值。当 ax 减没了，错位了，dx 才减少 1
+    jnz s
+    pop ax
+    pop dx
+    dec bx
+    jnz l
+    mov al, ah
+    out 61h, al
+    pop dx
+    ret
 ;--------------------------------------------------
 ;wait the game
 wait_game:
-	mov al,level[0]
-	mov ah,2ch
-	int 21h
-	add dh,al
-	mov bl,dh
-	cmp bl,3ch
-	jb wait_time
-	sub bl,60
+    mov al,level[0]
+    mov ah,2ch
+    int 21h
+    add dh,al
+    mov bl,dh
+    cmp bl,3ch
+    jb wait_time
+    sub bl,60
 wait_time:
-	mov ah,2ch
-	int 21h
-	cmp bl,dh
-	je wait_end
-	jmp wait_time
+    mov ah,2ch
+    int 21h
+    cmp bl,dh
+    je wait_end
+    jmp wait_time
 wait_end:
+	mov ah,1
+	int 16h
+	jz wait_quit
+	mov ah,0
+	int 16h
+	cmp al,'w'
+	je dir_w
+	cmp al,'s'
+	je dir_s
+	cmp al,'a'
+	je dir_a
+	cmp al,'d'
+	je dir_d
+dir_w:
+	mov al,0
+	jmp wait_dir
+dir_s:
+	mov al,1
+	jmp wait_dir
+dir_a:
+	mov al,2
+	jmp wait_dir
+dir_d:
+	mov al,3
+	jmp wait_dir
+wait_dir:
+	mov snake_dir[0],al
+wait_quit:
     ret 
 ;--------------------------------------------------
 ;初始化游戏
@@ -241,21 +269,22 @@ loop0:
     mov s_locate[442],ax
     mov ax,2048
     mov s_locate[444],ax
-	mov ax,0b870h
-	mov es,ax
-	mov al,level[0]
-	add al,30h
-	mov es:[2],al
+    mov ax,0b870h
+    mov es,ax
+    mov al,level[0]
+    add al,30h
 
-	mov al,level[0]
+    mov es:[2],al
+
+    mov al,level[0]
     ret
 ;--------------------------------------------------
 ;开始游戏
 ingame:
-	call wait_game
+    call wait_game
     call go_snake 
-	call music
-	call is_alive
+;    call music
+    call is_alive
     mov bx,offset game_flag
     mov dl,[bx]
     cmp dl,0
@@ -266,16 +295,16 @@ ingame:
     cmp dl,0
     je ingame
     call scores_increase
-	call food_create
-	mov dl,0
-	mov eat_food[0],dl
+    call food_create
+    mov dl,0
+    mov eat_food[0],dl
     jmp ingame
 end_ingame:
-	call end_deal
+    call end_deal
     ret
 ;--------------------------------------------------
 end_deal:
-	ret
+    ret
 ;--------------------------------------------------
 ;分数增加
 scores_increase:
@@ -296,7 +325,6 @@ increase:
     mov es:[si],al
     sub si,2
     loop increase
-    call food_create
     ret
 ;--------------------------------------------------
 ;将十进制数转化为字符型
@@ -324,56 +352,56 @@ trans_end:
 ;--------------------------------------------------
 ;判断食物是否被吃掉
 food_eat:
-	mov bl,food_y[0]
-	mov al,84
-	mul bl
-	mov dx,ax
-	mov bl,food_x[0]
-	mov bh,0
-	mov al,bl
-	mov bl,2
-	mul bl
-	add dx,ax
-	mov ax,dx
-	mov dx,snake_head[0]
-	cmp ax,dx
-	je eat
-	mov dl,0
-	mov eat_food[0],dl
-	jmp eat_end
-eat:	
-	mov dl,1
-	mov eat_food[0],dl
-	jmp eat_end
+    mov bl,food_y[0]
+    mov al,84
+    mul bl
+    mov dx,ax
+    mov bl,food_x[0]
+    mov bh,0
+    mov al,bl
+    mov bl,2
+    mul bl
+    add dx,ax
+    mov ax,dx
+    mov dx,snake_head[0]
+    cmp ax,dx
+    je eat
+    mov dl,0
+    mov eat_food[0],dl
+    jmp eat_end
+eat:    
+    mov dl,1
+    mov eat_food[0],dl
+    jmp eat_end
 eat_end:
     ret
 ;--------------------------------------------------
 is_alive:
-	mov al,game_flag[0]
-	cmp al,0
-	je alive_end
-	mov al,eat_food[0]
-	cmp al,1
-	je eated
-	mov ax,value_temp[8]
-	cmp ax,1
-	je alive
-	cmp ax,2048
-	je alive
-	mov al,0
-	mov game_flag[0],al
-	jmp alive_end
+    mov al,game_flag[0]
+    cmp al,0
+    je alive_end
+    mov al,eat_food[0]
+    cmp al,1
+    je eated
+    mov ax,value_temp[8]
+    cmp ax,1
+    je alive
+    cmp ax,2048
+    je alive
+    mov al,0
+    mov game_flag[0],al
+    jmp alive_end
 alive:
-	mov al,2
-	mov game_flag[0],al
-	jmp alive_end
+    mov al,2
+    mov game_flag[0],al
+    jmp alive_end
 eated:
-	mov ax,value_temp[8]
-	cmp ax,1
-	je alive
-	mov al,0
-	mov game_flag[0],al
-	jmp alive_end
+    mov ax,value_temp[8]
+    cmp ax,1
+    je alive
+    mov al,0
+    mov game_flag[0],al
+    jmp alive_end
 alive_end:
     ret
 ;--------------------------------------------------
@@ -391,20 +419,20 @@ go_snake:
     je right
 up:    
     cmp ax,84
-    jb snake_over
+    jb relay
     sub ax,84
     jmp food
 down:
     cmp ax,1008
-    jae snake_over
+    jae relay
     add ax,84
     jmp food
-left:	
+left:    
     push ax
     mov bl,84
     div bl
     cmp ah,0
-    je snake_over
+    je relay
     pop ax
     sub ax,2
     jmp food
@@ -413,37 +441,45 @@ right:
     mov bl,84
     div bl
     cmp ah,82
-    je snake_over
+    je relay
     pop ax
     add ax,2
     jmp food
+relay:
+	jmp snake_over
 food:
     mov bx,snake_head[0]
     mov snake_head[0],ax
     mov si,ax
-	push bx
-	mov bx,offset s_locate
+    push bx
+    mov bx,offset s_locate
     mov cx,[bx+si]
-	pop bx
+    pop bx
     mov s_locate[si],bx
     mov value_temp[8],cx
+	cmp cx,1
+	jne snake_over
 continue:
     mov cx,s_locate[bx]
     cmp cx,2048
     je compare
-	mov ax,bx
+    mov ax,bx
     mov bx,cx
     jmp continue
 compare:
-	mov value_temp[10],bx
-;	call food_eat
+    mov value_temp[10],bx
+	push bx
+	push ax
+    call food_eat
+	pop ax
+	pop bx
     mov dl,eat_food[0]
     cmp dl,1
     je go_end
-	mov dx,1
-	mov s_locate[bx],dx
+    mov dx,1
+    mov s_locate[bx],dx
     mov dx,2048
-	mov bx,ax
+    mov bx,ax
     mov s_locate[bx],dx
     jmp go_end
 snake_over:
@@ -456,84 +492,84 @@ go_end:
 ;画蛇
 draw_snake:
     mov ax,snake_head[0]
-	push ax
+    push ax
     mov bl,84
     div bl
-	mov coordinate_y,al
-	mov al,ah
-	mov ah,0
-	mov bl,2
-	div bl
-	mov coordinate_x,al
-	call locate
-	mov es,value_temp[0]
-	mov si,value_temp[2]
-	mov dl,'@'
-	mov es:[si],dl
-	pop ax
+    mov coordinate_y,al
+    mov al,ah
+    mov ah,0
+    mov bl,2
+    div bl
+    mov coordinate_x,al
+    call locate
+    mov es,value_temp[0]
+    mov si,value_temp[2]
+    mov dl,'@'
+    mov es:[si],dl
+    pop ax
 draw_body:
-	mov bx,ax
-	mov ax,s_locate[bx]
-	push ax
-	mov dl,84
-	div dl
-	mov coordinate_y,al
-	mov al,ah
-	mov ah,0
-	mov dl,2
-	div dl
-	mov coordinate_x,al
-	pop ax
-	cmp ax,2048
-	je draw_tail
-	push ax
-	call locate
-	mov es,value_temp[0]
-	mov si,value_temp[2]
-	mov dl,'-'
-	mov es:[si],dl
-	pop ax
-	jmp draw_body
-draw_tail:	
-	mov ax,bx
-	push ax
-	mov dl,84
-	div dl
-	mov coordinate_y,al
-	mov al,ah
-	mov ah,0
-	mov bl,2
-	div bl
-	mov coordinate_x,al
-	call locate
-	mov es,value_temp[0]
-	mov si,value_temp[2]
-	mov dl,'>'
-	mov es:[si],dl
-	pop ax
-	mov bx,value_temp[10]
-	cmp ax,bx
-	je draw_end
-	call clear_tail
-draw_end:	
+    mov bx,ax
+    mov ax,s_locate[bx]
+    push ax
+    mov dl,84
+    div dl
+    mov coordinate_y,al
+    mov al,ah
+    mov ah,0
+    mov dl,2
+    div dl
+    mov coordinate_x,al
+    pop ax
+    cmp ax,2048
+    je draw_tail
+    push ax
+    call locate
+    mov es,value_temp[0]
+    mov si,value_temp[2]
+    mov dl,'-'
+    mov es:[si],dl
+    pop ax
+    jmp draw_body
+draw_tail:    
+    mov ax,bx
+    push ax
+    mov dl,84
+    div dl
+    mov coordinate_y,al
+    mov al,ah
+    mov ah,0
+    mov bl,2
+    div bl
+    mov coordinate_x,al
+    call locate
+    mov es,value_temp[0]
+    mov si,value_temp[2]
+    mov dl,'>'
+    mov es:[si],dl
+    pop ax
+    mov bx,value_temp[10]
+    cmp ax,bx
+    je draw_end
+    call clear_tail
+draw_end:    
     ret
 ;--------------------------------------------------
 clear_tail:
-	mov ax,value_temp[10]
-	mov dl,84
-	div dl
-	mov coordinate_y,al
-	mov al,ah
-	mov ah,0
-	mov bl,2
-	div bl
-	mov coordinate_x,al
-	call locate
-	mov es,value_temp[0]
-	mov si,value_temp[2]
-	mov dl,' '
-	mov es:[si],dl
-	ret
+    mov ax,value_temp[10]
+    mov dl,84
+    div dl
+    mov coordinate_y,al
+    mov al,ah
+    mov ah,0
+    mov bl,2
+    div bl
+    mov coordinate_x,al
+    call locate
+    mov es,value_temp[0]
+    mov si,value_temp[2]
+    mov dl,' '
+    mov es:[si],dl
+    ret
 ;--------------------------------------------------
 ;随机放置食物
 food_create:
@@ -545,7 +581,7 @@ food_begin:
     mov dl,13
     div dl
     mov bx,offset coordinate_y
-	mov food_y[0],ah
+    mov food_y[0],ah
     mov [bx],ah    
     
     mov ah,0
@@ -555,12 +591,12 @@ food_begin:
     mov dl,43
     div dl
     mov bx,offset coordinate_x
-	mov food_x[0],ah
+    mov food_x[0],ah
     mov [bx],ah
-	
-	call locate
-	mov es,value_temp[0]
-	mov si,value_temp[2]
+    
+    call locate
+    mov es,value_temp[0]
+    mov si,value_temp[2]
     mov dl,' '
     cmp es:[si],dl
     je food_end
@@ -606,9 +642,9 @@ locate:
     mov ah,0
     mov si,ax
 
-	mov value_temp[0],es
-	mov value_temp[2],si
-	ret
+    mov value_temp[0],es
+    mov value_temp[2],si
+    ret
 ;--------------------------------------------------
 ;清屏函数
 clear:
